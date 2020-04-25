8e7dcd4b0bec4e188259bac1ff49a6d33e153464
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-04-25 18:51:07 -0400

    comunicando de hijo a padre

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  x = 'freezer!';
+  rpta: number = 0;
+
+  recibirResultado(e: any){
+    this.rpta = e;
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { CompaComponent } from './compa/compa.component';
+import { CompbComponent } from './compb/compb.component';
+import { CompcComponent } from './compc/compc.component';
+import { GokuComponent } from './goku/goku.component';
+import { GohanComponent } from './gohan/gohan.component';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculadoraComponent,
+    CompaComponent,
+    CompbComponent,
+    CompcComponent,
+    GokuComponent,
+    GohanComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
